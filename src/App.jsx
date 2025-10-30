2911f975e869c9679976608c7f5653607aefac51
SamuelMucham <samuelmucham0607@gmail.com>
2025-10-30 11:32:10 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,26 @@ function App() {
 
   return (
     <>
-      <h1 className={styles.titulo}>samuel</h1>
+      <nav className={styles.menu}>
+        <a href="#s1">home</a>
+        <a href="#s2">cards</a>
+        <a href="#s3">video</a>
+        <a href="#s4">contato</a>
+      </nav>
+      <main>
+        <section>
+          <h2>session1</h2>
+        </section>
+        <section>
+          <h2>session2</h2>
+        </section>
+        <section>
+          <h2>session3</h2>
+        </section>
+        <section>
+          <h2>session</h2>
+        </section>
+      </main>
     </>
   )
 }
