c8a44ed899d12ffe1daf6269dbe8f41e0112699f
SamuelMucham <samuelmucham0607@gmail.com>
2025-10-30 10:03:01 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import styles from'./App.module.css'
+
+function App() {
+
+//javascript
+
+  return (
+    <>
+      <h1 className={styles.titulo}>samuel</h1>
+    </>
+  )
+}
+
+export default App
